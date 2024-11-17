4335c88c0e9c34e8a1cde7873ad4ec9e68f1f4f9
abhilash <ammojusaiabhilash1507@gmail.com>
2024-11-17 19:56:50 +0530

    First commit to maven java project

---
+++ src/main/java/SEgroup/Batch1Java/Software.java
@@ -0,0 +1,10 @@
+package SEgroup.Batch1Java;
+
+public class Software {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");	// TODO Auto-generated method stub
+
+	}
+
+}
